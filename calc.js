690f2c3cc5f1f964c6ed87b71dd22c166975345d
Leomillan <leonardo.amillan@outlook.com>
2022-06-17 01:16:59 -0300

    BETA 01 - Calc JS

---
+++ calc.js
@@ -0,0 +1,36 @@
+
+function insert(num) {
+    var reg = /^\d.?\d*$/;
+    var display = document.getElementById('result').innerHTML
+    if (display.length == 0 && !reg.test(num)) {
+        return;
+    }
+    if ((display.includes('+') || display.includes('-') || display.includes('*') || display.includes('/')) && !reg.test(num)) {
+        if (!reg.test(display.slice(display.length - 1))) {
+            return;
+        }
+        calc()
+    }
+    var number = document.getElementById("result").innerHTML;
+    document.getElementById('result').innerHTML = number + num;
+}
+
+function clean() {
+    document.getElementById('result').innerHTML = "";
+}
+
+function calc() {
+    var result = document.getElementById("result").innerHTML;
+    if (result) {
+        document.getElementById('result').innerHTML = eval(result);
+    }
+}
+
+function back(){
+    var display = document.getElementById('result').innerHTML
+    var myArray = display.split("")
+    myArray.pop();
+    var output = myArray.join('');
+    document.getElementById('result').innerHTML = output;
+   
+}
